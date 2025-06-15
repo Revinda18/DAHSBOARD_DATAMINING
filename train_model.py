749b724f83799834b8be5960b0603c4077966c54
Revinda18 <revindavisma1811@gmail.com>
2025-06-15 20:37:47 +0700

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -7,15 +7,15 @@ from CONVERT_TO_PKL.feature_importance import show_feature_importance_page
 
 @st.cache_data
 def load_data():
-    df = pd.read_pickle(r"D:\SEMESTER 6\PENGGALIAN DATA\DASHBOARD\FILE_PKL\dataset_final_normalisasi.pkl")
+    df = pd.read_pickle(r"dataset_final.pkl")
     return df
 
 df = load_data()
 
 # Load model, scaler, dan fitur yang sudah disimpan
-model = joblib.load(r"D:\SEMESTER 6\PENGGALIAN DATA\DASHBOARD\FILE_PKL\model_spotify.pkl")
-scaler = joblib.load(r"D:\SEMESTER 6\PENGGALIAN DATA\DASHBOARD\FILE_PKL\scaler_spotify.pkl")
-feature_names = joblib.load(r"D:\SEMESTER 6\PENGGALIAN DATA\DASHBOARD\FILE_PKL\fitur_spotify.pkl")
+model = joblib.load(r"model_spotify.pkl)
+scaler = joblib.load(r"scaler_spotify.pkl")
+feature_names = joblib.load(r"fitur_spotify.pkl")
 
 st.sidebar.title("Spotify Dashboard")
 page = st.sidebar.radio("Pilih Halaman", ["EDA", "Model Klasifikasi", "Rekomendasi Lagu", "Feature Importance"])
