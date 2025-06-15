ff55adcbce6fb41e59e78ee601647e0dd997010f
Revinda18 <revindavisma1811@gmail.com>
2025-06-15 20:19:46 +0700

    Update halaman_utama.py

--- halaman_utama.py
+++ halaman_utama.py
@@ -1,34 +1,35 @@
 import streamlit as st
 import pandas as pd
 import joblib
+
 from feature_importance import show_feature_importance_page
 from overview import show_overview
 from song_recommendation import show_song_recommendation
 from favorite_song_prediction import show_label_analysis
 
+st.set_page_config(page_title="Spotify Dashboard", layout="wide")
+
 @st.cache_data
 def load_data():
-    df = pd.read_csv("D:/SEMESTER 6/PENGGALIAN DATA/DASHBOARD/DATASET/dataset.csv")
+    df = pd.read_csv("dataset.csv")  # Gunakan path relatif jika file ada di repo yang sama
     return df
 
 df = load_data()
 
-model = joblib.load("D:/SEMESTER 6/PENGGALIAN DATA/DASHBOARD/FILE_PKL/model_spotify.pkl")
+model = joblib.load("model_spotify.pkl")  # Path relatif
 fitur_model = [
     "popularity", "duration_ms", "explicit", "danceability", "energy",
     "key", "loudness", "mode", "speechiness"
 ]
 
 st.sidebar.title("Spotify Dashboard")
-page = st.sidebar.radio("Pilih Halaman", ["Overview Dashboard", "Aplikasi Prediksi Kesukaan Lagu Spotify", "Rekomendasi Lagu", "Feature Importance"])
+page = st.sidebar.radio("Pilih Halaman", [
+    "Overview Dashboard",
+    "Aplikasi Prediksi Kesukaan Lagu Spotify",
+    "Rekomendasi Lagu",
+    "Feature Importance"
+])
 
 if page == "Overview Dashboard":
     show_overview(df)
-elif page ==  "Aplikasi Prediksi Kesukaan Lagu Spotify":
-    show_label_analysis(df)
-elif page == "Feature Importance":
-    show_feature_importance_page(df, model, feature_names)
-elif page == "Rekomendasi Lagu":
-    show_song_recommendation(df)
-else:
-    st.title(f"Halaman {page} sedang dalam pengembangan")
+elif page =
